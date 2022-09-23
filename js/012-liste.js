77ebc2ec60e56807f6a40fbbe1f6d6f9edeb96a8
Umit Yildirim <umity@uia.no>
2022-09-24 01:17:45 +0200

    let qu

--- js/012-liste.js
+++ js/012-liste.js
@@ -1,4 +1,4 @@
-
+/*
 let ulDOM = document.querySelector("ul#list")
 let liDOM = document.createElement('li')
 
@@ -7,3 +7,10 @@ liDOM.innerHTML = "Kendi Olusturdugumuz oge"
 ulDOM.append(liDOM)
 
 ulDOM.prepend(liDOM)
+*/
+
+let Quidem = document.querySelector("#Quidem")
+Quidem.classList.add("text-primary")
+console.log(Quidem.classList)
+Quidem.classList.add("title")
+Quidem.classList.add("new info", "second class")
